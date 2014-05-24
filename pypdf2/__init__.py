3964282c68779b6a5c5db26cd10fd0165d546802
ciju <ciju.ch3rian@gmail.com>
2014-05-25 01:43:21 +0530

    update pypdf2 to expose needed classes

--- pypdf2/__init__.py
+++ pypdf2/__init__.py
@@ -1,4 +1,5 @@
-from .pdf import PdfFileReader, PdfFileWriter
+from .pdf import PdfFileReader, PdfFileWriter, PageObject, ContentStream
+from .generic import NameObject, DictionaryObject, DecodedStreamObject
 from .merger import PdfFileMerger
 from .pagerange import PageRange, parse_filename_page_ranges
 from ._version import __version__
