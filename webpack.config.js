86ab565d57e94ed9a889f9243eea85562a23d623
ciju <ciju.ch3rian@gmail.com>
2015-12-14 12:41:11 +0530

    add jQuery, lodash, pdfjs

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,19 @@
+var webpack = require('webpack');
+
 module.exports = {
   entry: "./static/js/app.js",
   output: {
     path: __dirname,
     filename: "./static/js/bundle.js"
   },
+  devtool: 'source-map',
+  resolve: {
+    moduleDirectories: ['node_modules', 'bower_components']
+  },
   module: {
     loaders: [
       { test: /\.css$/, loader: "style!css" },
-      { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader"}
+      { test: /\.js$/, exclude: /node_modules|bower_components/, loader: "babel-loader"}
     ]
   }
 };
